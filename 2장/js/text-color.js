e6e0eb0de96fbe6b4904941158e0d57dfa5c2c08
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2026-03-13 14:19:15 +0900

    feat: 2장(글자색 바꾸기, 시간 표시 예제)

---
+++ 2장/js/text-color.js
@@ -0,0 +1,6 @@
+// heading 요소를 선택하여 클릭 이벤트를 설정
+var heading = document.querySelector('#heading');
+// 클릭할 때마다 텍스트 색상을 빨간색과 파란색으로 번갈아 변경
+heading.onclick = function () {
+  heading.style.color = heading.style.color === 'red' ? 'blue' : 'red';
+};
